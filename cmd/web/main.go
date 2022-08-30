65635ede38f978b469f8878fcf9b878cdd4eedfe
raihaninfo <raihanmahmudi35@gmail.com>
2022-08-30 23:03:36 +0600

    ping route

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Dokander software")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on
+	fmt.Println("server started")
+	// fmt.Println("Dokander software")
 }
