eefc0bb8fd1dbd9814c6c0a45f0ea8997e0d5c7b
raihaninfo <raihanmahmudi35@gmail.com>
2022-09-08 20:28:16 +0600

    add model gorm tag

--- models/customers.go
+++ models/customers.go
@@ -1,12 +1,12 @@
 package models
 
 type Customers struct {
-	Id          int
+	Id          int `gorm:"primaryKey;autoIncrement:true;unique"`
 	Name        string
-	Email       string
+	Email       string `gorm:"unique"`
 	Mobile      string
 	DateOfBirth string
 	Reference   string
 	Address     string
-	Status      string
+	Status      int
 }
--- models/products.go
+++ models/products.go
@@ -1,13 +1,13 @@
 package models
 
 type Products struct {
-	Id              int
+	Id              int `gorm:"primaryKey;autoIncrement:true;unique"`
 	PurchasesDate   string
 	ProductName     string
-	Brand           string
-	Model           string
-	ProductType     string
-	Quantity        string
+	Brand           string `gorm:"nullable"`
+	Model           string `gorm:"nullable"`
+	ProductType     string `gorm:"nullable"`
+	Quantity        int
 	PurchasesAmount string
 	SellAmount      string
 	Status          int
